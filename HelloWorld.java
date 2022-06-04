aea2328bf05631369001533ba07c5cb45eafdfb2
shashiharavi <102820262+shashiharavi@users.noreply.github.com>
2022-06-04 20:04:30 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,8 +2,10 @@ public class HelloWorld {
 
 	public String getMessage(boolean bigger) {
 		if (bigger) {
+			system.out.println("Hello World")
 			return "Hello Universe!";
 		} else {
+			system.out.println("Hello World twice")
 			return "Hello World!";
 		}
 	}
